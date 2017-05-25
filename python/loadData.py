2876abcc2349200244441e007982eb823bf5513b
KJ <kjprice12@gmail.com>
2017-05-25 14:43:33 -0500

    add import statements to top of file

--- python/loadData.py
+++ python/loadData.py
@@ -1,11 +1,10 @@
-import sys
-print (sys.version)
-
-import os
-print (os.getcwd())
-
 import pandas as pd
 import numpy as np
+import sys
+import os
+
+print (sys.version)
+print (os.getcwd())
 
 housing_raw = pd.read_csv('data/housing-subset-5percent.csv')
 

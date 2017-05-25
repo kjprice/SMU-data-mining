fa0288c08b642eec8e71f8e8de2d72413ae3684c
KJ <kjprice12@gmail.com>
2017-05-24 20:29:49 -0500

    5% sample of full housing data

--- r/create-random-sample.r
+++ r/create-random-sample.r
@@ -1,6 +1,11 @@
 set.seed(5)
 
-numberRowsToRetrieve = round(nrow(housing_raw)*.10) # 10 percent
-random.sample = housing_raw[sample(1:nrow(housing_raw), numberRowsToRetrieve, replace=FALSE),]
+# take housing data
+housing_raw_a = read.csv('../data/ss13husa.csv')
+housing_raw_b = read.csv('../data/ss13husb.csv')
+housing_full = rbind(housing_raw_a, housing_raw_b)
 
-write.csv(random.sample, '../data/housing-subset-10percent.csv')
+numberRowsToRetrieve = round(nrow(housing_full)*.05) # 5 percent
+random.sample = housing_full[sample(1:nrow(housing_full), numberRowsToRetrieve, replace=FALSE),]
+
+write.csv(random.sample, '../data/housing-subset-5percent.csv')

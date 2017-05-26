75bd3bc2e3b445b764102d2b5d6f6a8e5acfa569
jnizzi <jnizzi@smu.edu>
2017-05-25 23:08:24 -0500

    Add Jen load data
    Add Jen load data

--- python/load_data.py
+++ python/load_data.py
@@ -10,3 +10,8 @@ os.chdir('/Users/kjprice/Library/Projects/smu/data-mining/Data Mining Project/')
 
 housing_raw = pd.read_csv('data/housing-subset-5percent.csv')
 
+
+
+
+#Jen - - some changes to load the data
+housing_raw = pd.read_csv('housing-subset-5percent.csv')

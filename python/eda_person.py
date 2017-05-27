b8bc84a3e64220fcda07da00c8f30c1b87a97fda
KJ <kjprice12@gmail.com>
2017-05-26 21:05:16 -0500

    Box Plot

--- python/eda_person.py
+++ python/eda_person.py
@@ -1,3 +1,13 @@
 from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+
+# QOI: Any features that seem to determine income (PINCP)?
+
+# Univariate plots
+
+### Barplot: income
+df.PINCP.plot.box(figsize=(10,12))
 
 scatter_matrix(df[dollarFeatures], figsize=(25, 20))
+
+

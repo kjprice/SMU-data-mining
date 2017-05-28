6160abf2a1382f4ca10efa1ae4ed9cd15defc322
KJ <kjprice12@gmail.com>
2017-05-27 19:33:41 -0500

    Move filesystem-specific code to its own file

--- python/load_data_person.py
+++ python/load_data_person.py
@@ -8,7 +8,3 @@ print (os.getcwd())
 
 person_raw = pd.read_csv('data/person-subset-2.5percent.csv')
 
-#Jen - - some changes to load the data
-os.chdir('c:/Users/nizzi/Documents/DataMiningProj/SMU-data-mining/data')
-person_raw = pd.read_csv('person-subset-2.5percent.csv')
-
